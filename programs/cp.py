f79d280693339c668b2c9af746d725d8e7f73c42
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-09 23:23:29 -0400

    Typo fix.

--- programs/cp.py
+++ programs/cp.py
@@ -17,7 +17,7 @@ def run(shell, args):
         if kernel.filesystem.is_file(src):
             if kernel.filesystem.is_dir(dest):
                 if args.verbose:
-                    shell.stdout.put("Copying %s to %s" %(src, dest)
+                    shell.stdout.put("Copying %s to %s" %(src, dest))
                 kernel.filesystem.copy(src, dest)
             else:
                 shell.stderr.put(dest + " is not a directory")
