a379ecb3c806887cc7fbae6260e6b76c96118554
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-09 23:36:17 -0400

    Made help actually work.

--- programs/help.py
+++ programs/help.py
@@ -2,9 +2,18 @@ import kernel.filesystem
 
 def run(shell, args):
     if len(args) >= 1:
-        x = kernel.filesystem.open_program(args[0])
-        if x:
-            shell.stdout.put(x.help())
+        name = args[0]
+        if name[0:2] == "./":
+            a = [shell.iabs_path(name)]
+        else:
+            paths = shell.get_var('PATH').split(':')
+            a = [kernel.filesystem.join_path(x, name) for x in paths]
+        program = False
+        for x in a:
+            program = kernel.filesystem.open_program(x)
+            if program:
+                shell.stdout.put(program.help())
+                break
         else:
             shell.stderr.put("%s: command not found" %name)
     else:
