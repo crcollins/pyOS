fb17b19a6934307caecb78fa03adbc8513f1450e
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-10 00:23:35 -0400

    Added basic list dir.

--- programs/ls.py
+++ programs/ls.py
@@ -1,2 +1,12 @@
+import kernel.filesystem
+
 def run(shell, args):
-    pass
+    if args:
+        path = args[0]
+    else:
+        path = shell.curpath
+    a = '\n'.join(kernel.filesystem.list_dir(path))
+    shell.stdout.put(a)
+
+def help():
+    return "HELP"
