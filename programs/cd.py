09318ccefae55e32dacfbd6b2d6d1f9a71dd023e
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-19 23:14:28 -0400

    Added incorrect dir catch.

--- programs/cd.py
+++ programs/cd.py
@@ -1,8 +1,13 @@
+import kernel.filesystem
+
 def run(shell, args):
     #hack to fix encapsulation
     shell = shell.parent
     if args:
-        shell.set_curpath(args[0])
+        if kernel.filesystem.is_directory(shell.iabs_path(args[0])):
+            shell.set_curpath(args[0])
+        else:
+            shell.stderr.write("%s: no such directory" %args[0])
 
 def help():
     a = """
