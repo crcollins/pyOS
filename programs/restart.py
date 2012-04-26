67d48e98eaf284c658cc22e1a8f7ea7940995a35
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-25 23:30:08 -0400

    Added a basic restart program.

--- programs/restart.py
+++ programs/restart.py
@@ -1,2 +1,14 @@
+from kernel.system import System
+
 def run(shell, args):
-    pass
+    System.state = -2
+
+def help():
+    a = """
+    Restart
+
+    Restarts the OS.
+
+    usage: restart
+    """
+    return a
