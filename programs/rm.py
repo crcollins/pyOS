1864d511d2e7c5b873ffd3bb95ea5840e5f0f174
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-26 00:11:39 -0400

    Added remove.

--- programs/rm.py
+++ programs/rm.py
@@ -1,2 +1,36 @@
+import argparse
+
+import kernel.filesystem as fs
+
+parser = argparse.ArgumentParser(add_help=False)
+pa = parser.add_argument
+pa('paths', type=str, nargs='*',)
+pa('-f', action="store_true", dest="force", default=False)
+pa('-r', action="store_true", dest="recursive", default=False)
+pa('-v', action="store_true", dest="verbose", default=False)
+
 def run(shell, args):
-    pass
+    args = parser.parse_args(args)
+    if len(args.paths) >= 1:
+        path = args.paths
+        if fs.is_file(path) or (fs.is_dir(path) and args.recursive):
+            if args.verbose:
+                shell.stdout.write("Removing %s" %(path))
+            fs.remove(path, recursive=args.recursive)
+        else:
+            if not args.recursive and fs.is_dir(path):
+                shell.stderr.write("%s is a directory" %(path))
+            elif not fs.is_file(path):
+                shell.stderr.write("%s is not a file" %(path))     
+    else:
+        shell.stderr.write("missing file operand")
+
+def help():
+    a = """
+    Remove
+
+    Removes the file/directory.
+
+    usage: rm [path]
+    """
+    return a
