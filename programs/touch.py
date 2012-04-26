26d853e9f4ac42d1fbe691b4deb30d078d396fe7
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-26 00:44:51 -0400

    Added touch.

--- programs/touch.py
+++ programs/touch.py
@@ -1,2 +1,20 @@
 def run(shell, args):
-    pass
+    if args:
+        path = shell.iabs_path(args[0])
+        if not kernel.filesystem.exists(path):
+            f = kernel.filesystem.open_file(path,'w')
+            f.close()
+        else:
+            shell.stderr.write("%s already exists" %(path))
+    else:
+        shell.stderr.write("missing file operand")
+
+def help():
+    a = """
+    Touch
+
+    Creates an empty file at the given path.
+
+    usage: touch [path]
+    """
+    return a
