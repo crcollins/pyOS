d51eda03a82ffb5a9a6cd77ad211116529083689
chris0076 <crcoll9955@northgeorgia.edu>
2012-04-26 00:39:20 -0400

    Added mkdir.

---
+++ programs/mkdir.py
@@ -0,0 +1,21 @@
+import kernel.filesystem
+
+def run(shell, args):
+    if args:
+        path = shell.iabs_path(args[0])
+        if not kernel.filesystem.exists(path):
+            kernel.filesystem.make_dir(path)
+        else:
+            shell.stderr.write("%s already exists" %(path))
+    else:
+        shell.stderr.write("missing directory operand")
+
+def help():
+    a = """
+    Make Directory
+
+    Creates a directory at the given path.
+
+    usage: mkdir [path]
+    """
+    return a
