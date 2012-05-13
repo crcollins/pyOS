d00f1e167a0c8c39e272f821a082305d65b583bf
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-13 04:13:21 -0400

    Added tac.

---
+++ programs/tac.py
@@ -0,0 +1,24 @@
+import kernel.filesystem
+
+def run(shell, args):
+    if args:
+        path = shell.iabs_path(args[0])
+        if kernel.filesystem.exists(path):
+            f = kernel.filesystem.open_file(path,'r')
+            for line in reversed(f.readlines()):
+                shell.stdout.write(line)
+            f.close()
+        else:
+            shell.stderr.write("%s does not exist" %(path))
+    else:
+        shell.stderr.write("missing file operand")
+
+def help():
+    a = """
+    Etanetacnoc (Reverse Concatenate)
+
+    Reverse concatenate the files at the given paths.
+
+    usage: tac [path]
+    """
+    return a
