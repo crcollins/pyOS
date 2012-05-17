c26c52b76e872b3eff1abcbcd782cf457d9803ec
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-16 20:15:39 -0400

    Rewrote the interpreter.

--- programs/interpreter.py
+++ programs/interpreter.py
@@ -11,13 +11,10 @@ def run(shell, args):
         data = raw_input("root@pyOS:%s$ "%shell.path)
         try:
             programs = eval_input(shell, data)
-            shells, programs = start_shells(shell, programs)
-            connect_shells(shells, programs)
+            shells = start_shells(shell, programs)
+            connect_shells(shells)
             for x in shells:
                 x.run()
-            for (program, args, cin, cout) in programs:
-                for x in cout:
-                    x.close()
         except IndexError:
             pass
 
@@ -41,47 +38,51 @@ def eval_input(shell, string):
 
         if program in shell.aliases:
             program = shell.aliases[program]
-        args, cin, cout = [], [], []
-        for part in pipeset:
+        args, cin, cout = [], None, None
+        #reversed so that the operators near the command take precedence
+        for part in pipeset[::-1]:
             p2 = part.lstrip("<> ")
-            if ">" in part:
-                cout.append(fs.open_file(p2, "w"))
-            elif ">>" in part:
-                cout.append(fs.open_file(p2, "a"))
+            if ">>" in part:
+                cout = ((p2, "a"))
+            elif ">" in part:
+                cout = ((p2, "w"))
             elif "<" in part:
-                cin.append(p2)
+                cin = p2
             else:
                 args.extend(part.split())
         final.append((program, args, cin, cout))
-    #((program, [arg0, arg1, ...], [Fcin0, Fcin1, ...], [Fcout0, Fcout1, ...]), ...)
+    #((program, [arg0, arg1, ...], cin, (cout, mode)), ...)
     return final
 
-def start_shells(shell, programs):
-    parent = shell
-    path = shell.path
+def start_shells(parent, programs):
+    path = parent.path
 
-    listing = []
-    newprograms = []
-    for (program, args, cin, cout) in programs:
-        newshell = System.new_shell(parent=parent, path=path,
-                         program=program, args=args)
-        #hack to convert the cin into streams from cat.
-        newcin = [System.new_shell(parent=parent, path=path,
-                         program="cat", args=[x]) for x in cin]
-        listing.append(newshell)
-        newprograms.append((program, args, newcin, cout))
-    return listing, newprograms
+    proper = []
+    for (programname, args, cin, cout) in programs:
+        #hack to convert cin into streams from cat.
+        if cin:
+            newcin = System.new_shell(parent=parent, path=path,
+                         program="cat", args=[cin])
+        else:
+            newcin = cin
 
-def connect_shells(shells, programs):
-    #connect all of the cins and couts
-    for i, (program, args, cins, couts) in enumerate(programs):
-        program = shells[i]
-        list((x.stdout.add(program.stdin, stream=True) for x in cins))
-        list((program.stdout.add(x.write, stream=False) for x in couts))
+        newshell = System.new_shell(parent=parent, stdin=newcin, path=path,
+                program=programname, args=args)
+
+        #hack to convert cout into streams to write
+        if cout:
+            newcout = System.new_shell(parent=parent, stdin=newcin, path=path,
+                         program="write", args=cout)
+        else:
+            newcout = cout
+        proper.extend([x for x in [newcin, newshell, newcout] if x])
+    #(Scin0, shell0, Scout0, Scin1 ...)
+    return proper
+
+def connect_shells(shells):
     #connect the seperate programs
-    for i, p1 in enumerate(shells[1:]):
-        p0 = shells[i]
-        p0.stdout.add(p1.stdin, stream=True)
+    for p0, p1 in zip(shells[:-1], shells[1:]):
+        p0.stdout.set_reader(p1)
 
 def help():
     return "HELP"
