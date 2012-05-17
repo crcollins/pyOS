21e887cd8d1524e5a870c520e7d0576deb0f25ea
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-16 20:14:53 -0400

    Added write.

---
+++ programs/write.py
@@ -0,0 +1,31 @@
+import kernel.filesystem
+
+def run(shell, args):
+    if args:
+        path = shell.iabs_path(args[0])
+        try:
+            if args[1] in ["a","w"]:
+                mode = args[1]
+            else:
+                shell.stderr.write("%s is not a valid file mode" %(args[1]))
+        except IndexError:
+            mode = 'w'
+        try:
+            f = kernel.filesystem.open_file(path,mode)
+            for line in shell.stdin.read():
+                f.write(line+"\n")
+            f.close()
+        except:
+            shell.stderr.write("file error")
+    else:
+        shell.stderr.write("missing file operand")
+
+def help():
+    a = """
+    Write
+
+    Writes the stdin to a file at the given path.
+
+    usage: write [path] [mode]
+    """
+    return a
