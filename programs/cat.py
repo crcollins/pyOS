7afd64d8881786b232b259d813771b0c7219af19
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-17 21:39:47 -0400

    Added stdin reading for cat.

--- programs/cat.py
+++ programs/cat.py
@@ -1,15 +1,19 @@
 import kernel.filesystem
 
 def run(shell, args):
-    if args:
-        path = shell.iabs_path(args[0])
-        if kernel.filesystem.exists(path):
-            f = kernel.filesystem.open_file(path,'r')
-            for line in f:
+    if args or shell.stdin:
+        for x in args:
+            path = shell.iabs_path(x)
+            try:
+                f = kernel.filesystem.open_file(path,'r')
+                for line in f:
+                    shell.stdout.write(line)
+                f.close()
+            except IOError:
+                shell.stderr.write("%s does not exist" %(path))
+        if shell.stdin:
+            for line in shell.stdin.read():
                 shell.stdout.write(line)
-            f.close()
-        else:
-            shell.stderr.write("%s does not exist" %(path))
     else:
         shell.stderr.write("missing file operand")
 
