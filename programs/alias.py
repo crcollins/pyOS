53986f53674cb2a6ae6d48fc6b2c384120cb85c2
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-19 16:24:57 -0400

    Simplified alias.

--- programs/alias.py
+++ programs/alias.py
@@ -3,13 +3,11 @@ def run(shell, args):
     shell = shell.parent
     if not args:
         for x in shell.aliases:
-            shell.stdout.write("%s %s" %(x, shell.aliases[x]))
+            shell.stdout.write("alias %s=%s" %(x, shell.aliases[x]))
     else:
         for item in args:
             if "=" in item:
-                x = item.index("=")
-                key = item[:x]
-                value = item[x+1:]
+                (key, value) = [x.strip() for x in item.split("=",1)]
                 shell.aliases[key] = value
             else:
                 shell.stderr.write("")
