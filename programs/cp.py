88f14755e23d29b368e9d02f0dd0f2bd3467d18e
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-19 17:21:57 -0400

    Fixed cp to work with multiple files.

--- programs/cp.py
+++ programs/cp.py
@@ -12,17 +12,18 @@ pa('-v', action="store_true", dest="verbose", default=False)
 def run(shell, args):
     args = parser.parse_args(args)
     if len(args.paths) >= 2:
-        src = shell.iabs_path(args.paths[0])
-        dest = shell.iabs_path(args.paths[1])
-        if kernel.filesystem.is_file(src) or args.recursive:
-            if kernel.filesystem.is_directory(dest):
+        dest = shell.iabs_path(args.paths[-1])
+        if kernel.filesystem.is_directory(dest) or len(args.paths) == 2:
+            for src in args.paths[:-1]:
+                src = shell.iabs_path(src)
                 if args.verbose:
                     shell.stdout.write("Copying %s to %s" %(src, dest))
-                kernel.filesystem.copy(src, dest, recursive=args.recursive)
-            else:
-                shell.stderr.write("%s is not a directory" %dest)
+                try:
+                    kernel.filesystem.copy(src, dest, recursive=args.recursive)
+                except IOError:
+                    shell.stderr.write("file error" %dest)
         else:
-            shell.stderr.write("%s is not a file" %src)
+            shell.stderr.write("%s is not a directory" %dest)
     else:
         shell.stderr.write("missing file operand")
 
