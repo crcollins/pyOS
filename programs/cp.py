257a668caa16478660130461f8def52e8835b536
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-19 02:45:20 -0400

    Fixed typos in cp.

--- programs/cp.py
+++ programs/cp.py
@@ -12,17 +12,17 @@ pa('-v', action="store_true", dest="verbose", default=False)
 def run(shell, args):
     args = parser.parse_args(args)
     if len(args.paths) >= 2:
-        src = shell.iabs_path(args[0])
-        dest = shell.iabs_path(args[1])
+        src = shell.iabs_path(args.paths[0])
+        dest = shell.iabs_path(args.paths[1])
         if kernel.filesystem.is_file(src) or args.recursive:
-            if kernel.filesystem.is_dir(dest):
+            if kernel.filesystem.is_directory(dest):
                 if args.verbose:
                     shell.stdout.write("Copying %s to %s" %(src, dest))
                 kernel.filesystem.copy(src, dest, recursive=args.recursive)
             else:
-                shell.stderr.write(dest + " is not a directory")
+                shell.stderr.write("%s is not a directory" %dest)
         else:
-            shell.stderr.write(src + " is not a file")
+            shell.stderr.write("%s is not a file" %src)
     else:
         shell.stderr.write("missing file operand")
 
