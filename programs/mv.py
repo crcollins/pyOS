775e4679daf572d08061b9c78508bf32d0084ac1
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-19 17:22:10 -0400

    Fixed mv to work with multiple files.

--- programs/mv.py
+++ programs/mv.py
@@ -12,12 +12,16 @@ pa('-v', action="store_true", dest="verbose", default=False)
 def run(shell, args):
     args = parser.parse_args(args)
     if len(args.paths) >= 2:
-        src = shell.iabs_path(args.paths[0])
-        dest = shell.iabs_path(args.paths[1])
-        if kernel.filesystem.is_directory(dest):
-            if args.verbose:
-                shell.stdout.write("Moving %s to %s" %(src, dest))
-            kernel.filesystem.move(src, dest)
+        dest = shell.iabs_path(args.paths[-1])
+        if kernel.filesystem.is_directory(dest) or len(args.paths) == 2:
+            for src in args.paths[:-1]:
+                src = shell.iabs_path(src)
+                if args.verbose:
+                    shell.stdout.write("Moving %s to %s" %(src, dest))
+                try:
+                    kernel.filesystem.move(src, dest)
+                except IOError:
+                    shell.stderr.write("file error" %dest)
         else:
             shell.stderr.write("%s is not a directory" %dest)
     else:
