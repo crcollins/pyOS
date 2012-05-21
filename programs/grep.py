33819c20b4c98af023398a79587e859ee06d64ae
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-21 02:35:21 -0400

    Added grep.

---
+++ programs/grep.py
@@ -0,0 +1,36 @@
+import re
+
+import kernel.filesystem
+
+def run(shell, args):
+    if args or shell.stdin:
+        try:
+            expression = re.compile(args[0])
+            for x in args[1:]:
+                path = shell.iabs_path(x)
+                try:
+                    f = kernel.filesystem.open_file(path,'r')
+                    for line in f:
+                        m = re.match(expression, line)
+                        if m is not None:
+                            shell.stdout.write(line.rstrip())
+                    f.close()
+                except IOError:
+                    shell.stderr.write("%s does not exist" %(path))
+            if shell.stdin:
+                for line in shell.stdin.read():
+                    shell.stdout.write(line)
+        except:
+            shell.stderr.write("invalid regular expression")
+    else:
+        shell.stderr.write("missing file operand")
+
+def help():
+    a = """
+    Grep
+
+    Search for lines in a file matching the pattern given.
+
+    usage: grep [pattern] [path]
+    """
+    return a
