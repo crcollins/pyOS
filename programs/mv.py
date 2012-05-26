d4c2caca19e05d8619444472defda41c3981a0db
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-26 03:00:51 -0400

    Setup mv to use the new parser.

--- programs/mv.py
+++ programs/mv.py
@@ -11,22 +11,24 @@ pa('-r', action="store_true", dest="recursive", default=False)
 pa('-v', action="store_true", dest="verbose", default=False)
 
 def run(shell, args):
+    parser.add_shell(shell)
     args = parser.parse_args(args)
-    if len(args.paths) >= 2:
-        dest = shell.iabs_path(args.paths[-1])
-        if kernel.filesystem.is_directory(dest) or len(args.paths) == 2:
-            for src in args.paths[:-1]:
-                src = shell.iabs_path(src)
-                if args.verbose:
-                    shell.stdout.write("Moving %s to %s" % (src, dest))
-                try:
-                    kernel.filesystem.move(src, dest)
-                except IOError:
-                    shell.stderr.write("file error" % (dest, ))
+    if not parser.help:
+        if len(args.paths) >= 2:
+            dest = shell.iabs_path(args.paths[-1])
+            if kernel.filesystem.is_directory(dest) or len(args.paths) == 2:
+                for src in args.paths[:-1]:
+                    src = shell.iabs_path(src)
+                    if args.verbose:
+                        shell.stdout.write("Moving %s to %s" % (src, dest))
+                    try:
+                        kernel.filesystem.move(src, dest)
+                    except IOError:
+                        shell.stderr.write("file error" % (dest, ))
+            else:
+                shell.stderr.write("%s is not a directory" % (dest, ))
         else:
-            shell.stderr.write("%s is not a directory" % (dest, ))
-    else:
-        shell.stderr.write("missing file operand")
+            shell.stderr.write("missing file operand")
 
 def help():
     return parser.format_help()
