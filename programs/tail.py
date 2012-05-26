8e620970e115f91848b7d237fadd6e1d437d0e2f
chris0076 <crcoll9955@northgeorgia.edu>
2012-05-26 03:17:12 -0400

    Setup tail to use the new parser.

--- programs/tail.py
+++ programs/tail.py
@@ -9,11 +9,12 @@ pa('paths', type=str, nargs='*',)
 pa('-n', action="store", type=int, dest="lineamount", default=5)
 
 def run(shell, args):
-    if args or shell.stdin:
-        args = parser.parse_args(args)
+    parser.add_shell(shell)
+    args = parser.parse_args(args)
+    if not parser.help:
         for x in args.paths:
             path = shell.iabs_path(x)
-            if args > 1:
+            if args.paths > 1 or shell.stdin:
                 shell.stdout.write("==> %s <==" % (x, ))
             try:
                 f = kernel.filesystem.open_file(path, 'r')
@@ -22,14 +23,16 @@ def run(shell, args):
                 f.close()
             except IOError:
                 shell.stderr.write("%s does not exist" % (path, ))
+        shell.stdout.write("")
         if shell.stdin:
             if args.paths:
                 shell.stdout.write("==> %% stdin %% <==")
             for line in shell.stdin.readlines()[-args.lineamount:]:
                 shell.stdout.write(line)
-        shell.stdout.write("")
-    else:
-        shell.stderr.write("missing file operand")
+            shell.stdout.write("")
+        else:
+            if not args.paths:
+                shell.stderr.write("missing file operand")
 
 def help():
     return parser.format_help()
