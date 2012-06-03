cdea38559bcea0f853243a371498ae6fe75b19c2
chris0076 <crcoll9955@northgeorgia.edu>
2012-06-02 21:29:19 -0400

    Rewrote grep.

--- programs/grep.py
+++ programs/grep.py
@@ -2,37 +2,61 @@ import re
 
 import kernel.filesystem
 
+
+desc = "Search for lines in a file matching the pattern given."
+parser = kernel.filesystem.Parser('grep', name="Grep", description=desc)
+pa = parser.add_argument
+pa('paths', type=str, nargs='*',)
+pa('-e', action="store", type=str, dest="pattern", default='')
+pa('-a', action="store_true", dest="all", default=False)
+pa('-i', action="store_true", dest="ignorecase", default=False)
+pa('-v', action="store_true", dest="invert", default=False)
+
 def run(shell, args):
-    if args or shell.stdin:
-        try:
-            expression = re.compile(args[0])
-            for x in args[1:]:
-                path = shell.sabs_path(x)
-                try:
-                    f = kernel.filesystem.open_file(path, 'r')
-                    for line in f:
-                        if re.findall(expression, line):
-                            shell.stdout.write(line.strip())
-                    f.close()
-                except IOError:
-                    shell.stderr.write("%s does not exist" % (path, ))
+    parser.add_shell(shell)
+    args = parser.parse_args(args)
+    if not parser.help:
+        if args.paths:
+            skip = 0
+            # re.IGNORECASE is a number
+            case = args.ignorecase * re.IGNORECASE
+            if not args.pattern:
+                pattern = re.compile(args.paths[0], case)
+                skip = 1
+            else:
+                pattern = re.compile(args.pattern, case)
+
+            paths = []
+            for path in args.paths[skip:]:
+                paths.extend(kernel.filesystem.list_glob(path))
+
+            for path in sorted(paths):
+                grep(shell, args, pattern, path)
+
             if shell.stdin:
                 for line in shell.stdin.read():
-                    if re.findall(expression, line):
+                    # use xor to invert the selection
+                    if bool(re.findall(pattern, line)) ^ args.invert:
                         shell.stdout.write(line.strip())
-            shell.stdout.write('')
-        except Exception as e:
-            print e
-            shell.stderr.write("invalid regular expression")
+            if not shell.stdout:
+                shell.stdout.write('')
+        else:
+            shell.stderr.write("missing file operand")
+
+def grep(shell, args, pattern, path):
+    newpath = shell.sabs_path(path)
+    if kernel.filesystem.is_file(path):
+        f = kernel.filesystem.open_file(newpath, 'r')
+        for line in f:
+            # use xor to invert the selection
+            if bool(re.findall(pattern, line)) ^ args.invert:
+                if shell.stdout:
+                    shell.stdout.write(line.rstrip())
+                else:
+                    shell.stdout.write("%s:%s" % (path, line.rstrip()))
+        f.close()
     else:
-        shell.stderr.write("missing file operand")
+        shell.stderr.write("%s does not exist" % (newpath, ))
 
 def help():
-    a = """
-    Grep
-
-    Search for lines in a file matching the pattern given.
-
-    usage: grep [pattern] [path]
-    """
-    return a
+    return parser.help_msg()
