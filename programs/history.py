f19b440c2cf311b21f3cf50c9fa7a4780c7fc82c
chris0076 <crcoll9955@northgeorgia.edu>
2012-06-16 17:34:45 -0400

    Added history.

---
+++ programs/history.py
@@ -0,0 +1,22 @@
+import kernel.filesystem
+
+desc = "Returns the command history."
+parser = kernel.filesystem.Parser('history', name="History", description=desc)
+pa = parser.add_argument
+pa('bla', type=str, nargs='*',)
+
+def run(shell, args):
+    parser.add_shell(shell)
+    args = parser.parse_args(args)
+    if not parser.help:
+        mlen = len(str(len(shell.prevcommands))) + 1
+        format = "{0:%dd}  {1}" % mlen
+        for i, line in enumerate(shell.prevcommands):
+            if not shell.stdout:
+                msg = format.format(i, line)
+            else:
+                msg = line
+            shell.stdout.write(msg)
+
+def help():
+    return parser.help_msg()
