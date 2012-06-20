18a0b4344c054fbae7a06e12d7d6e6f5677cbe81
chris0076 <crcoll9955@northgeorgia.edu>
2012-06-20 02:06:35 -0400

    Added bang replacement to interpreter.

--- programs/interpreter.py
+++ programs/interpreter.py
@@ -5,10 +5,12 @@ from kernel.system import System
 from kernel.constants import OSNAME, RUNNING, PIPECHAR, VARCHAR, \
         INCHAR, OUTCHAR, APPENDCHAR
 
-
 varparse = re.compile(r"\%s\w*" % (VARCHAR, ))
 stdioparse = re.compile(r"([%s%s]+\s*\w+)" % (OUTCHAR, INCHAR))
 quoteparse = re.compile(r"""(\"[^\"]*\"|\'[^\']*\'|\|)""")
+subparse = re.compile(r"""s(?P<lim>.)(.*?)(?P=lim)(.*?)(?P=lim)""")
+bangparse = re.compile(r"""(\!+[^!]*)""")
+# r"""(\!\!|(?<!\\)\![^!\s]*|\s+)"""
 
 def run(shell, args):
     user = shell.get_var('USER')
@@ -16,7 +18,7 @@ def run(shell, args):
         data = raw_input("%s@%s:%s$ "% (user, OSNAME, shell.path))
         data = data.strip()
         if data:
-            cleaned, command = clean_input(shell, data)
+            cleaned, command = shell_expansion(shell, data)
             shell.prevcommands.append(command)
 
             programs = eval_input(shell, cleaned)
@@ -34,21 +36,123 @@ def quote_split(string):
             a.append(x)
     return a
 
-def clean_input(shell, string):
+def get_hist(shell, value):
+    if ':' in value:
+        search, word = value.split(':')
+    elif value[0] in "$^*":
+        word = value
+        search = ''
+    else:
+        word = ''
+        search = value
+
+    command = ''
+    if search:
+        if search.startswith('!'):
+            command = shell.prevcommands[-1]
+        elif search.startswith('?'):
+            command = shell.hist_find(search[1:], False)
+        elif search.isdigit() or (search.startswith('-') and search[1:].isdigit()):
+            command = shell.prevcommands[int(search)]
+        else:
+            command = shell.hist_find(search)
+    
+    d = {
+        '$': lambda : slice(-1, None),
+        '^': lambda : slice(1, 2),
+        '*': lambda : slice(1, None),
+        '-': lambda x, y: slice(x, y)
+    }
+    execute = True
+    if word:
+        if command == '':
+            command = shell.prevcommands[-1]
+        # remove special chars
+        new = ''
+        options = ''
+        for char in word:
+            if char not in "eghqrtx&":
+                new += char
+            else:
+                if char == 'p':
+                    execute = False
+                options += char
+        subs = []
+        substitutesplit = re.split(subparse, new)
+        # magic number from spacing in search (text, sep, find, replace, text, ...)
+        word = ''.join(substitutesplit[::4])
+
+        if word[0] in "$^*":
+            slicer = d[word[0]]()
+        elif '-' in word:
+            split = [int(x) if x.isdigit() else None for x in word.split('-')]
+            if split[0] is None:
+                split[0] = 0
+            slicer = d['-'](split[0], split[1])
+        else:
+            slicer = int(word)
+    else:
+        slicer = slice(None)
+    return command.split()[slicer], execute
+
+def bang_replacement(shell, listing):
+    # http://www.softpanorama.org/Scripting/Shellorama/bash_command_history_reuse.shtml
+    '''
+    n           line n
+    -n          n lines back
+    !           last command
+    bla         last bla command
+    ?bla        last use of bla in command
+
+    If the word designator begins with $, %, ^, *, or - the : is not needed.
+    :0          command name
+    :^          first arg of command    
+    :n          arg n of command
+    :$          last arg of command
+    :*          all the args of command
+    :x-y        all args x through y. x=0
+
+    e           remove all but the suffix of a filename
+    g           make changes globally, use with s modifier, below
+    h           remove the last part of a filename, leaving the "head"
+    p           print the command but do not execute it
+    q           quote the generated text
+    r           remove the last suffix of a filename
+    s/old/new/  substitute new for old in the text. Any delimiter may be used. An & in the argument means the value of old. With empty old , use last old , or the most recent !? str ? search if there was no previous old
+    t           remove all but the last part of a filename, leaving the "tail"
+    x           quote the generated text, but break into words at blanks and newline
+    &           repeat the last substitution
+
+    '''
+    bang = []
+    execute = True
+    for part in listing:
+        if not part.startswith('"') and not part.startswith("'") and '!' in part:
+            for x in re.split(bangparse, part):
+                y = True
+                if x.startswith("!") and x != '!':
+                    x, y = get_hist(shell, x[1:])
+                    bang.extend(x)
+                elif x:
+                    bang.append(x)
+                if not y:
+                    execute = False
+        else:
+            bang.append(part)
+    if bang != listing:
+        shell.stdout.write(' '.join(bang))
+    return bang, execute
+
+def shell_expansion(shell, string):
+    # http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_03_04.html
     quote = quote_split(string)
 
-    #replace !!
+    # replace bang
     if shell.prevcommands:
-        bang = []
-        for x in quote:
-            if not x.startswith('"') and not x.startswith("'"):
-                bang.extend(x.replace("!!", shell.prevcommands[-1]).split())
-            else:
-                bang.append(x)
-        if bang != quote:
-            shell.stdout.write(' '.join(bang))
+        bang, execute = bang_replacement(shell, quote)
     else:
         bang = quote
+        execute = True
 
     #replace $vars
     cleaned = [re.sub(varparse, shell.get_var, xs) for xs in bang]
