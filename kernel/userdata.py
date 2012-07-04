291cfc5a63daa98814af57a8164fbdbc2ab9edc4
chris0076 <crcoll9955@northgeorgia.edu>
2012-07-04 12:40:20 -0400

    Made userdata useable.

--- kernel/userdata.py
+++ kernel/userdata.py
@@ -1,82 +1,129 @@
-import datetime
 import sqlite3
 
 from kernel.constants import USERDATAFILE
-
-def convert_many(start, *args):
-    if type(start) not in (list, set, tuple):
-        done = [(start, ) + args]
-    else:
-        done = [(x, ) + args for x in start]
-    return done
+from kernel.utils import convert_many
 
 def build_user_data_database():
-    addsql = 'INSERT INTO userdata VALUES (?, ?, ?)'
+    addsql = 'INSERT INTO userdata VALUES (?, ?, ?, ?, ?, ?)'
     tablesql = '''CREATE TABLE IF NOT EXISTS userdata (
-                    uid TEXT,
-                    gid TEXT,
+                    username TEXT,
+                    groupname TEXT,
+                    info TEXT,
+                    homedir TEXT,
+                    shell TEXT,
                     password TEXT)'''
 
     con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
-    items = (("root", "root", "d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1"), # pass
-            ("chris", "chris", "2744ccd10c7533bd736ad890f9dd5cab2adb27b07d500b9493f29cdc420cb2e0")) # me
+    items = (("root", "root", 'Root', '/', "/programs/interpreter",
+    "d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1"), # pass
+            ("chris", "chris", 'Chris', '/', "/programs/interpreter",
+    "2744ccd10c7533bd736ad890f9dd5cab2adb27b07d500b9493f29cdc420cb2e0")) # me
 
     with con:
         cur = con.cursor()
+        print tablesql
         cur.execute(tablesql)
         cur.executemany(addsql, items)
 
-def get_user_data(uid):
+def get_user_data(user):
     data = None
 
     con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
     with con:
         cur = con.cursor()
-        cur.execute("SELECT * FROM userdata WHERE uid = ?", (uid, ))
+        cur.execute("SELECT * FROM userdata WHERE username = ?", (user, ))
         data = cur.fetchone()
         if data:
             ## force data to be strings and not unicode
             data = tuple(str(x) if type(x) == unicode else x for x in data)
     return data
 
-def add_user(uid, gid, password):
-    addsql = 'INSERT INTO userdata VALUES (?, ?, ?)'
+def get_all_user_data():
+    data = None
 
     con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
     with con:
         cur = con.cursor()
-        cur.execute(tablesql)
-        cur.executemany(addsql, items)
+        cur.execute("SELECT * FROM userdata")
+        data = cur.fetchall()
+        if data:
+            ## force data to be strings and not unicode
+            data = [tuple(str(x) if type(x) == unicode else x for x in row) for row in data]
+    return data
 
-def delete_user(uid):
-    path = convert_many(uid)
-    delsql = 'DELETE FROM userdata WHERE uid = ?'
+#######################################
 
-    con = sqlite3.connect(METADATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
-    with con:
-        cur = con.cursor()
-        cur.executemany(delsql, uid)
+def add_user(user, group, info, homedir, shell, password):
+    addsql = 'INSERT INTO userdata VALUES (?, ?, ?, ?, ?, ?)'
 
-def get_gid(uid):
-    return get_user_data(uid)[1]
-
-def set_gid(uid, value):
     con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
     with con:
         cur = con.cursor()
-        cur.execute("UPDATE userdata SET gid = ? WHERE uid = ?", (value, uid))
+        cur.execute(addsql, (user, group, info, homedir, shell, password))
 
-def get_password(uid):
-    return get_user_data(uid)[1]
+def delete_user(user):
+    user = convert_many(user)
+    delsql = 'DELETE FROM userdata WHERE username = ?'
 
-def set_password(uid, value):
     con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
     with con:
         cur = con.cursor()
-        cur.execute("UPDATE userdata SET password = ? WHERE uid = ?", (value, uid))
+        cur.executemany(delsql, user)
 
-def correct_password(uid, password):
-    data = get_user_data(uid)
-    if data is None:
+def change_user(user, value):
+    pass
+
+#######################################
+
+def get_group(user):
+    return get_user_data(user)[1]
+
+def set_group(user, value):
+    con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
+    with con:
+        cur = con.cursor()
+        cur.execute("UPDATE userdata SET groupname = ? WHERE username = ?", (value, user))
+
+def get_info(user):
+    return get_user_data(user)[2]
+
+def set_info(user, value):
+    con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
+    with con:
+        cur = con.cursor()
+        cur.execute("UPDATE userdata SET info = ? WHERE username = ?", (value, user))
+
+def get_homedir(user):
+    return get_user_data(user)[3]
+
+def set_homedir(user, value):
+    con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
+    with con:
+        cur = con.cursor()
+        cur.execute("UPDATE userdata SET homedir = ? WHERE username = ?", (value, user))
+
+def get_shell(user):
+    return get_user_data(user)[4]
+
+def set_shell(user, value):
+    con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
+    with con:
+        cur = con.cursor()
+        cur.execute("UPDATE userdata SET shell = ? WHERE username = ?", (value, user))
+
+def get_password(user):
+    return get_user_data(user)[5]
+
+def set_password(user, value):
+    con = sqlite3.connect(USERDATAFILE,  detect_types=sqlite3.PARSE_DECLTYPES)
+    with con:
+        cur = con.cursor()
+        cur.execute("UPDATE userdata SET password = ? WHERE username = ?", (value, user))
+
+#######################################
+
+def correct_password(user, password):
+    try:
+        return get_password(user) == password
+    except:
         return 0
-    return data[2] == password
